eeaea9aea266ba4d9557591cd17977311e7818d7
Dave Dawson <davedawson@Daves-MBP.home>
2015-10-19 14:28:15 -0400

    [TextBox.jsx] Set up initial states on component

--- app/components/TextBox.jsx
+++ app/components/TextBox.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 var TextBox = React.createClass({
+	getInitialState: function() {
+    return {
+      text: ""
+    };
+  },
+  handleChange: function(event) {
+    this.setState({ text: event.target.value });
+  },
   render: function() {
     return (
       <section className="writer">
+      {this.state.text}
+      <textarea className="form-control"
+                  onChange={this.handleChange}>
+        </textarea>
         <form>
           <textarea id="text-editor" name="editor">
           </textarea>
