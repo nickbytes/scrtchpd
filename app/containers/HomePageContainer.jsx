d84acf4d2b28d1524d3dfb79f8519d59fa67321d
Dave Dawson <davedawson@Daves-MBP.home>
2015-10-19 13:15:57 -0400

    [app.css, HomePageContainer.jsx, hot-dev-app.html] Sloppily paste in existing HTML, JS & CSS to test existing set up in the Electron app.

--- app/containers/HomePageContainer.jsx
+++ app/containers/HomePageContainer.jsx
@@ -11,9 +11,20 @@ export default class HomePageContainer extends React.Component {
   render() {
     return (
       <div>
-        <h2>Home Page</h2>
-        <p>This is the homepage.</p>
-        <Link to="about">to About</Link>
+        <div className="border">
+          <ul>
+            <li className="clear"><a href="#modal-clear" className="call-modal"><span>&times;</span><em>Clear these notes</em></a></li>
+            <li className="info"><a href="#modal-info" className="call-modal"><span>?</span><em>sctrchpd.com info</em></a></li>
+          </ul>
+        </div>
+        <section className="writer">
+          <form>
+            <textarea id="text-editor" name="editor">
+            TEXTAREA</textarea>
+          </form>
+          <div id="preview"> </div>
+        </section>
+
       </div>
     );
   }
---
+++ app/js/index.js
@@ -0,0 +1,110 @@
+function inputFocus(){
+	jQuery('#content').focus();
+}
+
+function autoSave(){
+	$(function(){
+      $('textarea').uniform();
+      $('form').sisyphus({
+        timeout: 1,
+        onSave: function() {
+          // $.jnotify('Data are saved to Local Storage', 500);
+          // kludge on Uniform.js bug
+          setTimeout(function(){
+            $.uniform.update('textarea');
+          }, 200);
+          // console.log('sisyphus save');
+        },
+        onRestore: function() {
+          // $.jnotify('Data are restored from Local Storage', 500);
+          // kludge on Uniform.js bug
+          setTimeout(function(){
+            $.uniform.update('textarea');
+          }, 200);
+        },
+        onRelease: function() {
+          // $.jnotify('Data are removed from Local Storage', 'warning', 500);
+          // kludge on Uniform.js bug
+          setTimeout(function(){
+            $.uniform.update('textarea');
+          }, 500);
+        }
+      });
+    });
+}
+
+function codeEditor(){
+  CodeMirror.commands.autocomplete = function(cm) {
+    CodeMirror.showHint(cm, CodeMirror.hint.javascript);
+  };
+
+
+  // Waiting on the issue to format these individually: https://github.com/marijnh/CodeMirror/issues/1919
+  var editor = CodeMirror.fromTextArea(document.getElementById("text-editor"), {
+    mode: {
+      name: "markdown",
+      highlightFormatting: true
+    },
+    lineNumbers: false,
+    lineWrapping: true,
+    autofocus: true,
+    extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"},
+    // onLoad: onLoadEditor()
+    });
+
+    // Clear Editor
+    $('.clear-yes').on('click', function(){
+      editor.setValue("");
+      editor.clearHistory();
+      $("#text-editor").focus();
+      console.log('clear!');
+    });
+    editor.on("keyup", function() {
+      editor.save();
+      // console.log('saved to textarea');
+
+      // Add wrapper elements
+
+      if ($(".cm-formatting-header").length > 0) {
+        // $(".cm-formatting-header").parent().addClass('cm-header-wrap');
+        // console.log(this + 'exists');
+      }
+    });
+
+    $('.save').click(function(){
+      var existingText = $('#text-editor').val();
+      editor.setValue(existingText);
+      // console.log(existingText);
+    });
+  }
+
+function setCookie(){
+    if($.cookie('_scrtchpd') === 'true') {
+      // Do nothing, the cookie exists
+
+    } else {
+      // Set the cookie and add the initial class
+      $.cookie('_scrtchpd', 'true', { expires: 500 });
+      $('#modal-info').addClass('initial');
+    }
+
+  // $.cookie('newscrtchpd', 'Cookie Set', { expires: 500 });
+    $('.modal-close').on('click', function(){
+     $('#modal-info').removeClass('initial');
+     inputFocus();
+     // console.log('close');
+    });
+}
+
+ jQuery(document).ready(function() {
+  	inputFocus();
+    autoSave();
+    setCookie();
+  });
+ $(window).bind("load", function() {
+     codeEditor();
+  });
+
+
+
+console.log('test');
