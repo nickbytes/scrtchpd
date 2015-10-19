827a467d6d90637c358272e962a2b4839f1ce5bc
Dave Dawson <davedawson@Daves-MBP.home>
2015-10-19 15:44:15 -0400

    [package, TextBox.jsx] Add Codemirror and react-codemirror

--- app/components/TextBox.jsx
+++ app/components/TextBox.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
+var Codemirror = require('react-codemirror');
 
 var TextBox = React.createClass({
 	getInitialState: function() {
     return {
-      text: ""
+      text: "",
+      code: "// Code"
     };
   },
+  updateCode: function(newCode) {
+        this.setState({
+            code: newCode
+        });
+    },
   handleChange: function(event) {
     this.setState({ text: event.target.value });
   },
   render: function() {
+  	var options = {
+	    // lineNumbers: true
+    };
     return (
-      <section className="writer">
-      {this.state.text}
-      <textarea className="form-control"
-                  onChange={this.handleChange}>
-        </textarea>
-        <form>
-          <textarea id="text-editor" name="editor">
-          </textarea>
-        </form>
-        <div id="preview"> </div>
-      </section>
+    	<div>
+	    	<Codemirror value={this.state.code} onChange={this.updateCode} options={options} />
+	      <section className="writer">
+	      {this.state.text}
+	      <textarea className="form-control"
+	                  onChange={this.handleChange}>
+	        </textarea>
+	        <form>
+	          <textarea id="text-editor" name="editor">
+	          </textarea>
+	        </form>
+	        <div id="preview"> </div>
+	      </section>
+      </div>
     );
   }
 });
