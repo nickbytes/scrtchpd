9730b48468c66a75e45d91078cfd29649dfe9aa6
Dave Dawson <davedawson@Daves-MBP.home>
2015-12-07 11:04:13 -0500

    [textBox.jsx, hot-dev-app.html] Use react flare demo code to test pulling in data from DB

--- app/components/TextBox.jsx
+++ app/components/TextBox.jsx
@@ -4,16 +4,47 @@ var LocalStorageMixin = require('react-localstorage')
     require('../../node_modules/codemirror/mode/markdown/markdown.js')
     require('../../node_modules/codemirror/mode/gfm/gfm.js');
 
+var Messages = React.createClass({
+  render: function() {
+    var messageEls = this.props.messages.map(function(item, index) {
+      return (
+        <div key={index}><strong>{item.user}</strong>: {item.note}</div>
+      );
+    });
+    return <div className="messages">{messageEls}</div>;
+  },
+  // .getDOMNode is deprecated
+  // https://facebook.github.io/react/blog/2015/09/10/react-v0.14-rc1.html#dom-node-refs
+  scrollToBottom: function() {
+    // var el = ReactDOM.findDOMNode(this);
+    // el.scrollTop = el.scrollHeight;
+  },
+  componentDidMount: function() {
+    this.scrollToBottom();
+  },
+  componentDidUpdate: function() {
+    this.scrollToBottom();
+  }
+});
+
 var TextBox = React.createClass({
 	displayName: 'TextBox',
-  mixins: [LocalStorageMixin],
+  mixins: [LocalStorageMixin, ReactFireMixin],
 	getInitialState: function() {
     return {
       text: "",
       code: "Write something",
-      counter: 0
+      counter: 0,
+      messages: []
     };
     console.log('initial counter:' + this.state.counter);
+  },
+  addMessage: function(newMessage) {
+    this.firebaseRefs.messages.push(newMessage);
+  },
+  componentWillMount: function() {
+    /* this.bindAsArray(new Firebase('https://ultimate-donut.firebaseio.com/egghead/react/intro/messages'), 'messages'); */
+    this.bindAsArray(new Firebase('https://scrtchpd.firebaseio.com/notes'), 'messages');
   },
   updateCode: function(newCode) {
     this.setState({
@@ -21,6 +52,7 @@ var TextBox = React.createClass({
     });
     this.setState({counter: this.state.counter + 1});
     console.log('Updated counter:' + this.state.counter);
+    this.countChars();
   },
   clearText: function() {
     this.setState({
@@ -29,6 +61,10 @@ var TextBox = React.createClass({
   },
   handleChange: function(event) {
     this.setState({ text: event.target.value });
+  },
+  countChars: function(){
+    console.log('counting');
+    console.log(this.state.code.length);
   },
   render: function() {
   	var options = {
@@ -44,7 +80,10 @@ var TextBox = React.createClass({
     };
     return (
     	<div>
-      	
+      	<div>
+          <Messages messages={this.state.messages} />
+          
+        </div>
       	<section className="writer">
   	    	<Codemirror className="text-editor" id="text-editor" value={this.state.code} onChange={this.updateCode} options={options} />
       	</section>
